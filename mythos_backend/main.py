d84d3841841f8b968f6e2c47829af04d747ff29f
swizzcheeze <jmilbrath@gmail.com>
2026-05-29 00:55:35 -0500

    Update default port to 8013, fix healthcheck port reference

--- mythos_backend/main.py
+++ mythos_backend/main.py
@@ -154,7 +154,7 @@ if __name__ == "__main__":
     uvicorn.run(
         "mythos_backend.main:app",
         host="0.0.0.0",
-        port=8001,
+        port=8013,
         reload=True,
         log_level="info",
     )
